128023b92f7f99eec533a01d74a2299d67414378
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-05-20 12:48:33 +0200

    loading data from manefist json

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "ajil/app/controller/BaseController"
-], function (BaseController) {
+    "ajil/app/controller/BaseController", 
+    "sap/m/MessageBox", 
+    "sap/m/MessageToast", 
+], function (BaseController,MessageBox,MessageToast) {
 
     return BaseController.extend("ajil.app.controller.View1", {
 
@@ -21,13 +23,27 @@ sap.ui.define([
                });
 
         },
-        onBeforeRendering:function(){
-            this.getView().byId("idBtn").setVisible(true)
-        }, 
 
-        onAfterRendering:function(){
-            $("#idAnil--idBtn").hide(function(){$(this).fadeIn(5000)})
-        },
+        // onBeforeRendering:function(){
+        //     this.getView().byId("idBtn").setVisible(true)
+        // }, 
+
+        // onAfterRendering:function(){
+        //     $("#idAnil--idBtn").hide(function(){$(this).fadeIn(5000)})
+        // },
+        // onOrder:function(){
+        //     //alert("order is success")
+        //     MessageBox.confirm("confirmation", {
+        //         title:"confirm order", 
+        //         onClose:function(value){
+        //             if (value === "OK"){
+        //                 MessageToast.show("iam clicked")
+        //             }else{
+        //                 MessageBox.error("iam not clicked")
+        //             }
+        //         }
+        //     })
+        // },
          
     });
 })
